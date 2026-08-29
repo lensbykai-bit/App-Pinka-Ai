ed1d308887ddb806046976f494b1ecd9bed4ce3a
lensbykai-bit <lensbykai@gmail.com>
2026-08-30 00:03:07 +0700

    Fix analyzer warning in header

--- lib/widgets/header_network.dart
+++ lib/widgets/header_network.dart
@@ -1,6 +1,5 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import '../theme/app_theme.dart';
 
 class HeaderNetwork extends StatelessWidget {
   const HeaderNetwork({super.key});
